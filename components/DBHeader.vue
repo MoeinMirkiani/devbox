219039c0dbe8ade9d5b2f695d6fb8df0a192e2b2
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-06-06 22:14:23 +0330

    created header component for default layout

---
+++ components/DBHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="header flex items-center">
+        <DBContainer class="flex items-center">
+            <NuxtLink to="/" class="header__logo inline-block">
+                <NuxtIcon name="devbox-logo" filled />
+            </NuxtLink>
+        </DBContainer>
+    </header>
+</template>
+
+<script lang="ts" setup>
+import DBContainer from '~/components/UI/DBContainer.vue'
+</script>
+
+<style lang="scss">
+.header {
+    height: 68px;
+
+    &__logo {
+        width: 143px;
+        height: 30px;
+    }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
+        <DBHeader />
+
         <div class="page-content">
             <slot />
         </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+import DBHeader from '~/components/DBHeader.vue'
+</script>
