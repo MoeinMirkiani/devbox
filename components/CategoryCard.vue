e63c898eacbe640a7906d52ad484997460c18deb
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-07-06 18:40:38 +0330

    created CategoryCard component and added to main page's hero section

---
+++ components/CategoryCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <NuxtLink :to="{ path: '/' }" class="category h-[116px] flex flex-col justify-center items-center relative">
+        <div class="category__bg w-[216px] h-[188px] rounded-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0"></div>
+        <div class="category__icon w-16 h-16 z-10">
+            <NuxtIcon :name="icon" class="text-primary-green"/>
+        </div>
+        <span class="category__title text-22 text-gray-50 z-10">{{ category }}</span>
+    </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    category: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.category {
+    transition: all 0.2s linear;
+
+    &__bg {
+        transition: all 0.2s linear;
+    }
+
+    &:hover {
+        .category__bg {
+            background: theme('colors.primary-green');
+            box-shadow: -0.5px 8px 8px 0px rgba(42, 82, 77, 0.04), -2px 38px 29px 0px rgba(42, 82, 77, 0.06), -5px 100px 92px 0px rgba(42, 82, 77, 0.10);
+        }
+
+        .category__title,
+        .nuxt-icon {
+            color: theme('colors.white');
+        }
+    }
+}
+</style>
--- components/main/HeroSection.vue
+++ components/main/HeroSection.vue
@@ -3,17 +3,13 @@
         <AppContainer>
             <WordSwiper :words="categories" :fixed-word="$t('main.hero.fixed')" class="mb-6" />
 
-            <p class="hero__description text-24 font-light leading-9 max-w-[580px] mx-auto text-center opacity-80 mb-20">{{ $t('main.hero.description') }}</p>
+            <p class="hero__description text-24 font-light leading-9 max-w-[580px] mx-auto text-center opacity-80 mb-20">
+                {{ $t('main.hero.description') }}
+            </p>
 
             <div class="hero__categories flex justify-center divide-x divide-gray-90 bg-gray-98 rounded-6 py-6">
                 <div v-for="(category, i) in categories" :key="i" class="hero__category--wrapper flex-1">
-                    <NuxtLink :to="{ path: '/' }" class="hero__category h-[116px] flex flex-col justify-center items-center relative">
-                        <div class="hero__category--bg w-[216px] h-[188px] rounded-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0"></div>
-                        <div class="hero__category--icon w-16 h-16 z-10">
-                            <NuxtIcon :name="category.icon" class="text-primary-green"/>
-                        </div>
-                        <span class="hero__category--title text-22 text-gray-50 z-10">{{ category.title }}</span>
-                    </NuxtLink>
+                    <CategoryCard :category="category.title" :icon="category.icon" />
                 </div>
             </div>
         </AppContainer>
@@ -23,6 +19,7 @@
 <script setup lang="ts">
 import AppContainer from '~/components/UI/AppContainer.vue'
 import WordSwiper from '~/components/WordSwiper.vue'
+import CategoryCard from "~/components/CategoryCard.vue"
 
 const categories: object[] = [
     {
@@ -47,27 +44,3 @@ const categories: object[] = [
     }
 ]
 </script>
-
-<style lang="scss" scoped>
-.hero {
-    &__category {
-        transition: all 0.2s linear;
-
-        &--bg {
-            transition: all 0.2s linear;
-        }
-
-        &:hover {
-            .hero__category--bg {
-                background: theme('colors.primary-green');
-                box-shadow: -0.5px 8px 8px 0px rgba(42, 82, 77, 0.04), -2px 38px 29px 0px rgba(42, 82, 77, 0.06), -5px 100px 92px 0px rgba(42, 82, 77, 0.10);
-            }
-
-            .hero__category--title,
-            .nuxt-icon {
-                color: theme('colors.white');
-            }
-        }
-    }
-}
-</style>
