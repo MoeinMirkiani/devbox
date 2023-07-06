87609628b5a3607e9206cf899b2791eb91df5a33
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-07-06 13:46:53 +0330

    added hover state to AppButton and removed focus state

--- components/UI/AppButton.vue
+++ components/UI/AppButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="bg-primary-green text-white text-18 px-5 py-4 rounded-[10px] w-full focus:bg-primary-shade sm:w-max sm:px-12">
+    <button class="bg-primary-green text-white text-18 px-5 py-4 rounded-[10px] w-full hover:bg-primary-shade sm:w-max sm:px-12">
         <slot />
     </button>
 </template>
