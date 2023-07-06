44ecbeafb70114964c1ce24c10f51b37f96ed344
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-07-06 18:48:22 +0330

    created categories banner and added to main page's HeroSection

---
+++ components/category/CategoriesBanner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="hero__categories flex justify-center divide-x divide-gray-90 bg-gray-98 rounded-6 py-6">
+        <div v-for="(category, i) in categories" :key="i" class="hero__category--wrapper flex-1">
+            <CategoryCard :category="category.title" :icon="category.icon" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import CategoryCard from "~/components/category/CategoryCard.vue"
+
+defineProps({
+    categories: {
+        type: Array,
+        required: true
+    }
+})
+</script>
--- components/main/HeroSection.vue
+++ components/main/HeroSection.vue
@@ -7,11 +7,7 @@
                 {{ $t('main.hero.description') }}
             </p>
 
-            <div class="hero__categories flex justify-center divide-x divide-gray-90 bg-gray-98 rounded-6 py-6">
-                <div v-for="(category, i) in categories" :key="i" class="hero__category--wrapper flex-1">
-                    <CategoryCard :category="category.title" :icon="category.icon" />
-                </div>
-            </div>
+            <CategoriesBanner :categories="categories" />
         </AppContainer>
     </section>
 </template>
@@ -19,7 +15,7 @@
 <script setup lang="ts">
 import AppContainer from '~/components/UI/AppContainer.vue'
 import WordSwiper from '~/components/swiper/WordSwiper.vue'
-import CategoryCard from '~/components/category/CategoryCard.vue'
+import CategoriesBanner from "~/components/category/CategoriesBanner.vue"
 
 const categories: object[] = [
     {
