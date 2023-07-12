57dc07f3dd840c25ff9758d96e48ef3777cecccb
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-07-12 16:12:40 +0330

    added hover state to AppHeader's navigation items

--- pages/images/index.vue
+++ pages/images/index.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="images-page__wrapper">IMAGES</div>
+    <div class="images-page__wrapper">
+        <AppContainer>IMAGES</AppContainer>
+    </div>
 </template>
 
 <script lang="ts" setup>
+import AppContainer from "~/components/UI/AppContainer.vue"
+
 useHead(({
     title: 'Images'
 }))
