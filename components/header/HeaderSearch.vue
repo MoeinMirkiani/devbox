86616e5d0d5fceca908ccfd1c985e2383ddeb45f
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-07-22 19:26:30 +0330

    AppHeader search input now updates query params using debounce

--- components/header/HeaderSearch.vue
+++ components/header/HeaderSearch.vue
@@ -4,8 +4,10 @@
             <span class="text-50 font-light text-gray-30">{{ pageTitle }}</span>
         </div>
 
-        <div class="header-search__input relative">
+        <div class="header-search__input w-[calc(100%-210px)] relative">
             <input
+                v-model="query"
+                @input="updateQuery"
                 type="text"
                 class="outline-none w-full h-full bg-gray-95 rounded-4 pl-6 pr-22 py-5 text-18 text-gray-30"
                 placeholder="Search"
@@ -17,21 +19,33 @@
 </template>
 
 <script lang="ts" setup>
+//@ts-ignore
+import debounce from 'lodash.debounce'
 import IconButton from "~/components/UI/IconButton.vue"
 
 const route = useRoute()
+const router = useRouter()
 
 const pageTitle = computed(() => {
     const title = route.path.replace('/', '')
     return title.charAt(0).toUpperCase() + title.slice(1)
 })
+
+const query = ref('')
+
+const updateQuery = debounce(() => {
+    router.push({
+        path: '',
+        query: {
+            search: query.value
+        }
+    })
+}, 500)
 </script>
 
 <style lang="scss" scoped>
 .header-search {
     &__input {
-        width: calc(100% - 210px);
-
         input {
             &::placeholder {
                 color: theme('colors.gray-60');
