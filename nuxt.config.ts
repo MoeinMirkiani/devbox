8a34dcce9b5274733553ef25b5893079391cede6
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-09-23 13:58:04 +0330

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,12 @@ export default defineNuxtConfig({
         }
     },
 
+    supabase: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_KEY,
+        redirect: false
+    },
+
     modules: [
         '@nuxtjs/eslint-module',
         '@nuxtjs/tailwindcss',
