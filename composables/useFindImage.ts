07a1fb60351c0c48d000d93ad318d3d9888dc79c
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-10-05 14:23:49 +0330

    removed unnecessary composables

--- composables/useFindImage.ts
+++
@@ -1,5 +0,0 @@
-import { images } from "~/db/images"
-
-export const useFindImage = (id: string) => {
-    return images.find((image) => image.id === id)
-}
--- pages/images/[id].vue
+++ pages/images/[id].vue
@@ -15,5 +15,4 @@ import AppContainer from "~/components/UI/AppContainer.vue"
 const route = useRoute()
 
 const id = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id
-const image = useFindImage(id)
 </script>
