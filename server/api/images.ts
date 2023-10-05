d7b1872332a5c48f30a1232afb59f8b7c4da69b5
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-10-05 11:21:07 +0330

    added sorting and pagination to images api route

--- server/api/images.ts
+++ server/api/images.ts
@@ -1,7 +1,16 @@
 import { serverSupabaseClient } from '#supabase/server'
+import {QueryObject} from "ufo";
 
 export default defineEventHandler(async event => {
     const client = await serverSupabaseClient(event)
-    const { data } = await client.from('images').select()
+    const queries = getQuery(event)
+    const currentPage: number = Number(queries.currentPage) || 1
+    const perPage: number = Number(queries.perPage) || 9
+    const { data } = await client
+        .from('images')
+        .select()
+        .order('id')
+        .range((currentPage - 1) * perPage, currentPage * perPage - 1)
+
     return data
 })
