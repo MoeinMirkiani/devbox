a814eb2277bd7ca176ce747f244834cdaea2e2e1
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-10-21 16:00:22 +0330

    removed types folder and created the contracts folder to manage types and interfaces

--- contracts/types/Image.ts
+++ contracts/types/Image.ts
@@ -1,13 +1,15 @@
-import { Source } from "~/contracts/types/Source"
+import type { Source } from "~/contracts/types/Source"
 
 export type Image = {
-    id: string
-    source: Source
-    title: string
-    file: string
-    resolution: string
-    size: string
-    ratio: string
-    dimension: string
+    id: string,
+    source: Source,
+    title: string,
+    file: string,
+    resolution: string,
+    size: string,
+    ratio: string,
+    dimension: string,
     format: string
 }
+
+export type Images = Image[]
