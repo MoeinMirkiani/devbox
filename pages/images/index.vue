38c6302209b33c5e2a3af21c766f40efa85f2e40
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-10-23 18:08:26 +0330

    removed unnecessary statements

--- pages/images/index.vue
+++ pages/images/index.vue
@@ -2,7 +2,7 @@
     <div class="images-page__wrapper pt-8 pb-16 px-20 bg-gray-98 min-h-[calc(100vh-248px)] flex justify-center items-center">
         <AppContainer>
             <ImageList :items="images" />
-            <SpinnerIcon ref="loadMore" v-if="hasNextPage" class="mx-auto mt-10" />
+            <SpinnerIcon v-if="hasNextPage" ref="loadMore" class="mx-auto mt-10" />
         </AppContainer>
     </div>
 </template>
@@ -24,7 +24,6 @@ definePageMeta({
 })
 
 // Variables
-const loading = ref<boolean>(false)
 const images = ref<Images>([])
 const currentPage = ref<number>(1)
 const perPage = ref<number>(9)
@@ -37,11 +36,8 @@ const fetchImages = async () => {
         const { data } = await ImageService.list(currentPage.value, perPage.value)
         images.value = images.value.concat(data.value)
         hasNextPage.value = !(data.value.length < perPage.value)
-        console.log(data.value)
     } catch (e) {
         console.log(e)
-    } finally {
-        loading.value = false
     }
 }
 
