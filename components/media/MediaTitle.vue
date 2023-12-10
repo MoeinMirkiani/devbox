8f8b001aee510f50d4333036d6c0dc5da184d301
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-12-10 16:51:15 +0330

    created MediaTitleProps interface and added to MediaTitle component

--- components/media/MediaTitle.vue
+++ components/media/MediaTitle.vue
@@ -3,22 +3,13 @@
         <h1 class="media-title__title mb-3 text-24 text-gray-30">{{ title }}</h1>
         <span class="media-title__source text-16 text-gray-50">
             {{ $t('images.from') }}
-            <NuxtLink :to="source.link" class="text-link">{{ source.title }}</NuxtLink>
+            <NuxtLink :to="source.link" class="text-link">{{ source.name }}</NuxtLink>
         </span>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { Source } from '~/contracts/types/Source'
+import { type MediaTitleProps } from "~/contracts/components/media/MediaTitleProps"
 
-defineProps({
-    title: {
-        type: String,
-        required: true
-    },
-    source: {
-        type: Object as PropType<Source>,
-        required: true
-    }
-})
+defineProps<MediaTitleProps>()
 </script>
