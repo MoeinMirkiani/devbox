64468ee9a3ec78c4c4fe40a04cd56a87fb96df36
Moein Mirkiani <moein.mirkiani@gmail.com>
2023-12-17 17:41:52 +0330

    minor style update

--- components/images/ImagePreview.vue
+++ components/images/ImagePreview.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="image-preview flex flex-col items-center">
-        <div class="image-preview__wrapper rounded-4 border border-gray-90 p-5 mb-5 flex justify-center items-center">
-            <img :src="image" :alt="alt" class="object-contain" />
+        <div class="image-preview__wrapper rounded-4 border border-gray-90 p-5 mb-5 h-[372px] flex justify-center items-center">
+            <img :src="image" :alt="alt" class="object-contain w-full h-full" />
         </div>
 
         <div class="image-preview__license flex items-center gap-2">
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts" setup>
-import { type ImagePreviewProps } from "~/contracts/components/images/ImagePreviewProps"
+import type { ImagePreviewProps } from "~/contracts/components/images/ImagePreviewProps"
 
 defineProps<ImagePreviewProps>()
 </script>
--- components/media/MediaTitle.vue
+++ components/media/MediaTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="media-title">
-        <h1 class="media-title__title mb-3 text-24 text-gray-30">{{ title }}</h1>
+        <h1 class="media-title__title mb-3 text-24 font-medium text-gray-30">"{{ title }}"</h1>
         <span class="media-title__source text-16 text-gray-50">
             {{ $t('images.from') }}
             <NuxtLink :to="source.link" class="text-link">{{ source.name }}</NuxtLink>
