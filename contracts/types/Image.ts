c14be40c8e6313169d4a4537f0d9ce947966ac3e
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-16 21:24:57 +0330

    modified Image interface

--- contracts/types/Image.ts
+++ contracts/types/Image.ts
@@ -1,18 +1,18 @@
 import type { Source } from "~/contracts/types/Source"
 
-export type Image = {
-    id: string,
-    databaseId: number,
-    title: string,
-    featuredImage: string,
-    resolution: string,
-    dimension: string,
-    size: string,
-    ratio: string,
-    format: string,
-    isFree: boolean,
-    source: Source,
-    file: string,
+export interface Image {
+    id: string
+    databaseId: number
+    title: string
+    featuredImage: string
+    resolution: string
+    dimension: string
+    size: string
+    ratio: string
+    format: 'PNG' | 'JPG'
+    isFree: boolean
+    source: Source
+    file: string
 }
 
 export type Images = Image[]
