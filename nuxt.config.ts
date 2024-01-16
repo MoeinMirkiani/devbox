ca70a455a876e1e03e025a61860f326c5770e50a
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-16 20:32:27 +0330

    updated i18n config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,5 +19,30 @@ export default defineNuxtConfig({
         '@nuxtjs/i18n',
         'nuxt-icons',
         'nuxt-swiper'
-    ]
+    ],
+
+    i18n: {
+        locales: [
+            {
+                code: 'en-US',
+                iso: 'en-US',
+                file: 'en-US.json',
+                name: 'English',
+                dir: 'ltr'
+            }
+        ],
+        lazy: true,
+        strategy: 'no_prefix',
+        detectBrowserLanguage: false,
+        langDir: 'locales/',
+        defaultLocale: 'en-US',
+        types: 'composition',
+        pages: undefined,
+        dynamicRouteParams: false,
+        skipSettingLocaleOnNavigate: true,
+        compilation: {
+            strictMessage: false
+        },
+        vueI18n: './i18n.config.ts'
+    }
 })
