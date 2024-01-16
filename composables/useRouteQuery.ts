1e60ba75c8c10130d4b2e133f1a4dbf937f23e4e
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-16 22:04:08 +0330

    removed unnecessary composables

--- composables/useRouteQuery.ts
+++
@@ -1,7 +0,0 @@
-export function useRouteQuery(name: string): ComputedRef<string> {
-    const route = useRoute()
-    return computed(() => {
-        const query = route.query[name]
-        return Array.isArray(query) ? query[0] || '' : query || ''
-    })
-}
