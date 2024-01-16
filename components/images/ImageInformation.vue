419a66f619acb27b698dc38a452ac5839272c61e
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-16 21:21:18 +0330

    updated template to use iterable data

--- components/images/ImageInformation.vue
+++ components/images/ImageInformation.vue
@@ -4,24 +4,8 @@
             <p class="text-gray-30 font-medium">{{ $t('images.info') }}</p>
 
             <ul class="image-information__list flex flex-col gap-3">
-                <li class="image-information__item">
-                    <MediaInfo :label="$t('images.image.res')" :value="resolution" is-large />
-                </li>
-
-                <li class="image-information__item">
-                    <MediaInfo :label="$t('images.image.size')" :value="size" is-large />
-                </li>
-
-                <li class="image-information__item">
-                    <MediaInfo :label="$t('images.image.ratio')" :value="ratio" is-large />
-                </li>
-
-                <li class="image-information__item">
-                    <MediaInfo :label="$t('images.image.dimension')" :value="dimension" is-large />
-                </li>
-
-                <li class="image-information__item">
-                    <MediaInfo :label="$t('images.image.format')" :value="format.toUpperCase()" is-large />
+                <li v-for="item in info" :key="item.label" class="image-information__item">
+                    <MediaInfo :label="item.label" :value="item.value" is-large />
                 </li>
             </ul>
         </div>
@@ -38,9 +22,37 @@
 
 <script lang="ts" setup>
 import type { ImageInformationProps } from "~/contracts/components/images/ImageInformationProps"
+import type { MediaInfoProps } from "~/contracts/components/UI/MediaInfoProps"
 
 
-defineProps<ImageInformationProps>()
+const props = defineProps<ImageInformationProps>()
+
+
+// Variables
+const { t } = useI18n()
+
+const info = ref<MediaInfoProps[]>([
+    {
+        label: t('images.image.res'),
+        value: props.resolution
+    },
+    {
+        label: t('images.image.size'),
+        value: props.size
+    },
+    {
+        label: t('images.image.ratio'),
+        value: props.ratio
+    },
+    {
+        label: t('images.image.dimension'),
+        value: props.dimension
+    },
+    {
+        label: t('images.image.format'),
+        value: props.format
+    }
+])
 </script>
 
 <style lang="scss" scoped>
