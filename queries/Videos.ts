b30ce04a64479db946f6992d8f9bd03f270ac7e4
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-28 16:25:27 +0330

    created Videos query for graphql

---
+++ queries/Videos.ts
@@ -0,0 +1,26 @@
+export const VideosQuery: string = `
+query Videos($first: Int!, $after: String!, $keyword: String!) {
+  videos(first: $first, after: $after, where: { search: $keyword }) {
+    edges {
+      node {
+        id
+        featuredImage {
+          node {
+            mediaItemUrl
+          }
+        }
+        acfVideo {
+          resolution
+          duration
+          size
+          ratio
+        }
+      }
+    }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+  }
+}
+`
