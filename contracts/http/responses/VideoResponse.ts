d3090333a71f7e7f647f2ce962121d8dcd5bbcaa
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-29 12:08:00 +0330

    updated VideosResponse interface

--- contracts/http/responses/VideoResponse.ts
+++ contracts/http/responses/VideoResponse.ts
@@ -4,6 +4,6 @@ import type { PageInfo } from '~/contracts/http/PageInfo'
 export interface VideoResponse extends Video {}
 
 export interface VideosResponse {
-    images: VideoListItem[],
+    items: VideoListItem[],
     pageInfo: PageInfo
 }
