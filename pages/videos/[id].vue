37939e7b0fba5e3b61e4b46a1bda5a75493298f2
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-29 11:48:40 +0330

    added api call ti fetch single video

--- pages/videos/[id].vue
+++ pages/videos/[id].vue
@@ -2,19 +2,37 @@
     <div class="media min-h-[calc(100vh-148px)] pt-8 pb-16 px-20">
         <AppContainer>
             <ExploreMore :title="$t('videos.explore')">
-                {{ id }}
+                {{ video }}
             </ExploreMore>
         </AppContainer>
     </div>
 </template>
 
 <script lang="ts" setup>
+import VideoService from "~/services/VideoService"
+import type { Video, VideoListItem } from "~/contracts/types/Video"
+
+
 // Composables
 const id = useRouteInfo({ location: 'param', name: 'id' })
 
 
 // Variables
+const video = ref<Video>({} as Video)
+const title = ref<string>('')
+const more = ref<VideoListItem[]>([])
 
 
 // Methods
+const fetchVideoDetail = async () => {
+    const { data } = await VideoService.detail(id.value)
+    video.value = data.value
+    title.value = video.value.title
+}
+
+useSeoMeta({
+    title: () => title.value
+})
+
+await fetchVideoDetail()
 </script>
