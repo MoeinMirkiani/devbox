e4e09cb93092ff5e3197dabefd253e92ea88f518
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-01-31 14:41:09 +0330

    added vidstack related config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
+import { vite as vidstack } from 'vidstack/plugins'
+
 export default defineNuxtConfig({
+    vue: {
+        compilerOptions: {
+            isCustomElement: (tag: any) => tag.startsWith('media-'),
+        }
+    },
+
+    vite: {
+        plugins: [
+            // Include filter to only check specific files for components and styles.
+            vidstack({ include: /player\// }),
+        ],
+    },
+
     css: ['@/assets/styles/main.scss'],
 
     runtimeConfig: {
