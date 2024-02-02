48df813e5059ec582aa90e0b16de04bdff2b79c5
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-02 14:10:42 +0330

    updated SoundCard props

--- pages/sounds/index.vue
+++ pages/sounds/index.vue
@@ -2,11 +2,23 @@
     <PageSearch :title="$t('sounds.title')" />
 
     <div class="videos-page__wrapper pt-8 pb-16 px-20 bg-gray-98 min-h-[calc(100vh-248px)] flex justify-center">
-        <AppContainer>SOUNDS</AppContainer>
+        <AppContainer>
+            <SoundCard
+                title="White Rabbit"
+                :file="sound"
+                :poster="poster"
+                format="MP3"
+                size="24KB"
+            />
+        </AppContainer>
     </div>
 </template>
 
 <script lang="ts" setup>
+import poster from '~/assets/images/main--sounds-sample1.jpg'
+import sound from '~/assets/sounds/test.mp3'
+
+
 const { t } = useI18n()
 
 useHead(({
