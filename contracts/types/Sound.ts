292ac5762e5d57bb908c4f7e7e9a555fe2a29d2e
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-05 11:36:03 +0330

    created Sound interface

--- contracts/types/Sound.ts
+++ contracts/types/Sound.ts
@@ -2,3 +2,10 @@ export interface SoundDownloadItem {
     label: string
     link: string
 }
+
+export interface Sound {
+    title: string
+    featuredImage: string
+    size: string
+    files: SoundDownloadItem[]
+}
