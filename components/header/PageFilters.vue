edd301ac0d0adfc8aa4a7869be1a1895906ed3b3
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-07 12:50:30 +0330

    created PageFilters initial template

---
+++ components/header/PageFilters.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="page-filters bg-[rgba(255,255,255,0.4)] shadow-header px-20">
+        <AppContainer class="h-25 pb-4 pt-5 flex">
+            <div id="page-title" class="header-filters__title flex w-[210px]">
+                <span class="text-50 font-light text-gray-30">{{ props.title }}</span>
+            </div>
+        </AppContainer>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type { PageFiltersProps } from "~/contracts/components/header/PageFiltersProps"
+
+
+const props = defineProps<PageFiltersProps>()
+
+
+// Variables
+const route = useRoute()
+const router = useRouter()
+
+
+// Methods
+</script>
+
+<style lang="scss" scoped></style>
+
---
+++ contracts/components/header/PageFiltersProps.ts
@@ -0,0 +1,4 @@
+export interface PageFiltersProps {
+    title: string
+}
+
