3817567c313a95bcc1b93a682a62e514e935de18
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-07 12:23:49 +0330

    updated avatars initial page template

--- pages/avatars/index.vue
+++ pages/avatars/index.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="videos-page__wrapper">
-        <AppContainer>AVATARS</AppContainer>
+    <div class="avatars-page__wrapper pt-8 pb-16 px-20 bg-gray-98 min-h-[calc(100vh-248px)] flex justify-center">
+        <AppContainer>
+            Avatars
+        </AppContainer>
     </div>
 </template>
 
 <script lang="ts" setup>
-import AppContainer from "~/components/UI/AppContainer.vue"
+const { t } = useI18n()
 
 useHead(({
-    title: 'Avatars'
+    title: t('avatars.title')
 }))
-
-definePageMeta({
-    layout: 'search'
-})
 </script>
