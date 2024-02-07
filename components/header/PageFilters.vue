2dae1deb8b98eb222404b3c223d9316e44b57158
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-07 13:25:53 +0330

    added filters' name

--- components/header/PageFilters.vue
+++ components/header/PageFilters.vue
@@ -5,7 +5,17 @@
                 <span class="text-50 font-light text-gray-30">{{ props.title }}</span>
             </div>
 
-            <div class="filters w-[calc(100%-210px)] h-full bg-gray-98 rounded-5"></div>
+            <div class="filters w-[calc(100%-210px)] h-full bg-gray-98 rounded-5">
+                <div class="filters__banner h-full flex items-center divide-x divide-gray-90">
+                    <span
+                        v-for="filter in props.filters"
+                        :key="filter.name"
+                        class="filter-indicator inline-flex flex-grow justify-center items-center h-full text-center text-18 text-gray-60"
+                    >
+                        {{ filter.name }}
+                    </span>
+                </div>
+            </div>
         </AppContainer>
     </div>
 </template>
