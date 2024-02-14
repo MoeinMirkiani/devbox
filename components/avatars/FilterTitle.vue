ca4ca24edc661bf3fbeef6312534035486df88ed
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-14 17:46:24 +0330

    created FilterTitle

---
+++ components/avatars/FilterTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="h-16 flex justify-center items-center text-18 font-medium text-gray-50 opacity-80">
+        <span>{{ props.name }}</span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type { FilterTitleProps } from "~/contracts/components/avatars/FilterTitleProps"
+
+
+// Macros
+const props = defineProps<FilterTitleProps>()
+</script>
