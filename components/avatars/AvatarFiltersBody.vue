e26482fe3796cde2e619952cadb93f2444803f3b
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-14 17:40:00 +0330

    added AvatarFilterGroup

--- components/avatars/AvatarFiltersBody.vue
+++ components/avatars/AvatarFiltersBody.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="w-full bg-gray-98 rounded-5 shadow-card">
         <div class="w-full flex divide-x-2 divide-gray-95">
-            <div v-for="(filter, i) in props.filters" :key="i" class="flex-1">
-                <div class="h-16 flex justify-center items-center text-18 font-medium text-gray-50">
-                    <span>{{ filter.name }}</span>
-                </div>
-
-                <div v-for="(item, j) in filter.items" :key="j">{{ item.name }}</div>
-            </div>
+            <AvatarFilterGroup
+                v-for="(filter, index) in props.filters"
+                :key="index"
+                :name="filter.name"
+                :items="filter.items"
+            />
         </div>
     </div>
 </template>
