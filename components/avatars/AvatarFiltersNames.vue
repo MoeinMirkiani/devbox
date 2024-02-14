d4ef4a4bfcab8049973eefd31899557f41094251
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-14 16:49:33 +0330

    created AvatarFiltersNames

---
+++ components/avatars/AvatarFiltersNames.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full h-full bg-gray-98 rounded-5 relative cursor-pointer flex divide-x-2 divide-gray-95">
+        <div v-for="(name, index) in props.names" :key="index" class="flex-1 flex justify-center items-center text-18 font-medium text-gray-50">
+            <span>{{ name }}</span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type { AvatarFiltersNamesProps } from "~/contracts/components/avatars/AvatarFiltersNamesProps"
+
+// Macros
+const props = defineProps<AvatarFiltersNamesProps>()
+</script>
