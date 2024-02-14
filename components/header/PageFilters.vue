6559c1f077352b43dc7a880031d2824c0a8c2c6d
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-14 12:43:22 +0330

    accepts filters through slot

--- components/header/PageFilters.vue
+++ components/header/PageFilters.vue
@@ -1,15 +1,9 @@
 <template>
-    <div class="page-filters bg-[rgba(255,255,255,0.4)] shadow-header px-20">
+    <div class="bg-[rgba(255,255,255,0.4)] shadow-header px-20">
         <AppContainer class="h-25 pb-4 pt-5 flex">
             <PageTitle :title="props.title" />
 
-            <div class="filters w-[calc(100%-210px)] h-fit bg-gray-98 rounded-5 relative">
-                <div class="filters__trigger h-16 flex items-stretch divide-x divide-gray-90 cursor-pointer">
-                    <div v-for="filter in props.filters" :key="filter.name" class="flex flex-1 justify-center items-center text-18 font-medium text-gray-50 opacity-80">
-                        <span>{{ filter.name }}</span>
-                    </div>
-                </div>
-            </div>
+            <slot />
         </AppContainer>
     </div>
 </template>
