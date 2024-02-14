4aa5fe8ddf69bfd36f38cf71f07e031d39d3ebb1
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-14 17:46:36 +0330

    added FilterTitle

--- components/avatars/AvatarFilterGroup.vue
+++ components/avatars/AvatarFilterGroup.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="flex-1">
-        <div class="h-16 flex justify-center items-center text-18 font-medium text-gray-50 opacity-80">
-            <span>{{ props.name }}</span>
-        </div>
+        <FilterTitle :name="props.name" class="flex-1" />
 
         <div class="px-6 pt-4 pb-9">
             <div v-for="(item, j) in props.items" :key="j">{{ item.name }}</div>
--- components/avatars/AvatarFiltersNames.vue
+++ components/avatars/AvatarFiltersNames.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="w-full h-full bg-gray-98 rounded-5 relative cursor-pointer flex divide-x-2 divide-gray-95">
-        <div v-for="(name, index) in props.names" :key="index" class="flex-1 flex justify-center items-center text-18 font-medium text-gray-50">
-            <span>{{ name }}</span>
-        </div>
+        <FilterTitle v-for="(name, index) in props.names" :key="index" :name="name" class="flex-1" />
     </div>
 </template>
 
 <script lang="ts" setup>
 import type { AvatarFiltersNamesProps } from "~/contracts/components/avatars/AvatarFiltersNamesProps"
+
 
 // Macros
 const props = defineProps<AvatarFiltersNamesProps>()
