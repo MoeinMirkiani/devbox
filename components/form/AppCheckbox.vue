f3fd20b62b64b9eec4e57b58b779730dcbea6bf6
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-28 15:16:34 +0330

    added checkmark icon to AppCheckbox

--- components/form/AppCheckbox.vue
+++ components/form/AppCheckbox.vue
@@ -1,13 +1,33 @@
 <template>
-    <div class="checkbox">
+    <div class="checkbox flex items-center">
         <input
             v-model="model"
             :id="id"
             :value="props.value"
             type="checkbox"
+            class="
+                relative
+                appearance-none
+                w-5
+                h-5
+                border-2
+                border-primary-green
+                rounded-1
+                checked:bg-primary-green
+                before:content-['\2714']
+                before:font-serif
+                before:text-16
+                before:text-[transparent]
+                checked:before:text-white
+                before:absolute
+                before:top-1/2
+                before:left-1/2
+                before:-translate-x-1/2
+                before:-translate-y-1/2
+            "
         />
 
-        <label :for="id" class="text-18 text-gray-30">{{ props.label }}</label>
+        <label :for="id" class="text-18 text-gray-30 pl-2">{{ props.label }}</label>
     </div>
 </template>
 
