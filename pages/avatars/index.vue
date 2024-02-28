6e2ac3d2be63bb6908371b80e07223279a338574
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-28 17:52:35 +0330

    update: filters api call

--- pages/avatars/index.vue
+++ pages/avatars/index.vue
@@ -1,83 +1,36 @@
 <template>
     <PageFilters :title="$t('avatars.title')" :filters>
-        <AvatarFilters />
+        <AvatarFilters :filters />
     </PageFilters>
 
     <div class="avatars-page__wrapper pt-8 pb-16 px-20 bg-gray-98 min-h-[calc(100vh-248px)] flex justify-center">
         <AppContainer>
-            Avatars
         </AppContainer>
     </div>
 </template>
 
 <script lang="ts" setup>
+import AvatarService from "~/services/AvatarService"
+import type { Filter } from "~/contracts/types/Filter"
+
+
+// Composables
 const { t } = useI18n()
 
 useHead(({
     title: t('avatars.title')
 }))
 
+
 // Variables
-const filters = [
-    {
-        name: 'Gender',
-        items: [
-            {
-                name: 'Male',
-                slug: 'male'
-            },
-            {
-                name: 'Female',
-                slug: 'female'
-            }
-        ]
-    },
-    {
-        name: 'Age',
-        items: [
-            {
-                name: 'Male',
-                slug: 'male'
-            },
-            {
-                name: 'Female',
-                slug: 'female'
-            },
-            {
-                name: 'Male',
-                slug: 'male'
-            },
-            {
-                name: 'Female',
-                slug: 'female'
-            }
-        ]
-    },
-    {
-        name: 'Format',
-        items: [
-            {
-                name: 'Male',
-                slug: 'male'
-            },
-            {
-                name: 'Female',
-                slug: 'female'
-            }
-        ]
-    },
-    {
-        name: 'Color',
-        items: [
-            {
-                name: 'Male',
-                slug: 'male'
-            },
-            {
-                name: 'Female',
-                slug: 'female'
-            }
-        ]
-    }
-]
+const filters = ref<Filter[]>()
+
+
+// Methods
+const fetchFilters = async () => {
+    const { data } = await AvatarService.filters()
+    filters.value = data.value
+}
+
+await fetchFilters()
 </script>
