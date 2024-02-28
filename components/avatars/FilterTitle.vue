6231fd6ed0ad1c68eab89a0e1f28a311b96cca03
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-02-28 17:56:17 +0330

    update: capitalized title

--- components/avatars/FilterTitle.vue
+++ components/avatars/FilterTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-16 flex justify-center items-center text-18 font-medium text-gray-50 opacity-80">
+    <div class="h-16 flex justify-center items-center capitalize text-18 font-medium text-gray-50 opacity-80">
         <span>{{ props.name }}</span>
     </div>
 </template>
