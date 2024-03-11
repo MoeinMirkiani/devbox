df35481ed4e04e5a91647825f0ae6337a3000296
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-03-11 20:04:54 +0330

    feat: add logo list to logos page

---
+++ components/logos/LogoList.vue
@@ -0,0 +1,16 @@
+<template>
+    <NoResults v-if="!props.items.length && !props.loading" class="h-full" />
+
+    <div v-else class="logos flex items-start flex-wrap gap-x-4 gap-y-5">
+        {{ props.items }}
+    </div>
+
+    <SpinnerIcon v-if="props.loading" class="mx-auto mt-10" />
+</template>
+
+<script setup lang="ts">
+import type { LogoListProps } from "~/contracts/components/logos/LogoListProps"
+
+const props = defineProps<LogoListProps>()
+</script>
+
