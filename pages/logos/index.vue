da9ad1e50ca934a8f1ae6a4299851405fb6bf47a
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-03-11 19:10:25 +0330

    feat: add page search to logos page

--- pages/logos/index.vue
+++ pages/logos/index.vue
@@ -1,17 +1,16 @@
 <template>
-    <div class="videos-page__wrapper">
+    <PageSearch :title="$t('logos.title')" />
+
+    <div class="videos-page__wrapper pt-8 pb-16 px-20 bg-gray-98 min-h-[calc(100vh-248px)] flex justify-center">
         <AppContainer>LOGOS</AppContainer>
     </div>
 </template>
 
 <script lang="ts" setup>
-import AppContainer from "~/components/UI/AppContainer.vue"
+// Composables
+const { t } = useI18n()
 
 useHead(({
-    title: 'Logos'
+    title: t('logos.title')
 }))
-
-definePageMeta({
-    layout: 'search'
-})
 </script>
